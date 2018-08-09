9593613e4b65bcad19a7ae440000b173e808f9d4
Derick Cardin <derickc50@gmail.com>
2018-08-09 23:05:22 +0000

    Lesson 10-12

--- app.js
+++ app.js
@@ -72,19 +72,53 @@
 //bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for everyone else!';
 
 //LESSON 9//
-var btns = document.querySelectorAll('#book-list.delete');
+//var btns = document.querySelectorAll('#book-list.delete');
 
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        const li = e.target.parentElement;
-        li.parentNode.removeChild(li)
+//Array.from(btns).forEach(function(btn){
+  //  btn.addEventListener('click', function(e){
+    //    const li = e.target.parentElement;
+      //  li.parentNode.removeChild(li);
         
-    });
+//    });/
+//});
+
+//const link = document.querySelector('#page-banner a');
+
+//link.addEventListener('click', function(e){
+  //  e.preventDefault();
+    //console.log('navigation to', e.target.textContent, 'was prevented');
+//});
+
+//LESSON 10//
+const list = document.querySelector('#book-list ul');
+
+//delete books//
+list.addEventListener('click', function(e){
+    if(e.target.className =='delete'){
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
 });
 
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
+//add books//
+const addForm = document.forms['add-book'];
+addForm.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log('navigation to', e.target.textContent, 'was prevented');
-})
+    const value= addForm.querySelector('input[type="text"]').value;
+
+//create element//
+const li = document.createElement('li');
+const bookName = document.createElement('span');
+const deleteBtn = document.createElement('span');
+
+//add content//
+deleteBtn.textContent = 'delete';
+bookName.textContent = value;
+
+//append to dom//
+li.appendChild(bookName);
+li.appendChild(deleteBtn);
+
+list.appendChild(li);
+
+});
