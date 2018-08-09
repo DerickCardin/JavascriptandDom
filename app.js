dd173eb8e7c56610f45e334dc489c54aac9cb80f
Derick Cardin <derickc50@gmail.com>
2018-08-09 22:17:16 +0000

    Lesson7-9

--- app.js
+++ app.js
@@ -42,11 +42,49 @@
 //bookList.innerHTML += '<p>This is how you add HTML</p>';
 
 //Lesson 6//
-const banner = document.querySelector('#page-banner');
+//const banner = document.querySelector('#page-banner');
 
-console.log('#page-banner node type is:',banner.nodeType);
-console.log('#page-banner node name is:',banner.nodeName);
-console.log('#page-banner has child nodes:',banner.hasChildNodes());
+//console.log('#page-banner node type is:',banner.nodeType);
+//console.log('#page-banner node name is:',banner.nodeName);
+//console.log('#page-banner has child nodes:',banner.hasChildNodes());
 
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
+//const clonedBanner = banner.cloneNode(true);
+//console.log(clonedBanner);
+
+
+//Lesson 7//
+//const bookList = document.querySelector('#book-list');
+//console.log('the parent node is:', bookList.parentNode);
+//console.log('the parent element is:', bookList.parentElement.parentElement);
+
+//console.log(bookList.children);
+
+//LESSON 8//
+//const bookList = document.querySelector('#book-list');
+
+//console.log('book-list next sibling is:', bookList.nextSibling);
+//console.log('book-list next element sibling is:', bookList.nextElementSibling);
+
+//console.log('book-list previous sibling is:', bookList.previousSibling);
+//console.log('book-list previous element sibling is:', bookList.previousElementSibling);
+
+
+//bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for everyone else!';
+
+//LESSON 9//
+var btns = document.querySelectorAll('#book-list.delete');
+
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li)
+        
+    });
+});
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('navigation to', e.target.textContent, 'was prevented');
+})
